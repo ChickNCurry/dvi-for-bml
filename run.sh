1a9b3d5b09e1e8eacae4209d0d5a6cb398c6806c
Max Burzer <maxi.burzer@gmail.com>
2024-11-20 12:33:37 +0100

    fix git

--- run.sh
+++
@@ -1,8 +0,0 @@
-python run.py --multirun \
-common.h_dim=128 \
-common.num_layers=6 \
-common.non_linearity=SiLU,Tanh \
-control_function.is_cross_attentive=False,True \
-set_encoder.aggregation=max,mean \
-decoder.has_det_path=False,True \
-hydra/launcher=submitit_slurm \
