e459df835cd929bbdb327ce74bf92d87f9c5931b
Max Burzer <maxi.burzer@gmail.com>
2024-11-24 20:30:10 +0100

    update config for new run

--- runs/run.sh
+++ runs/run.sh
@@ -1,15 +1,15 @@
 python run.py --multirun \
 hydra=gpu \
-common.z_dim=2,4,8,16 \
-common.h_dim=64 \
+common.z_dim=2,4,6,8 \
+common.h_dim=32,64,96,128 \
 common.num_layers=6 \
 common.non_linearity=SiLU \
-set_encoder.aggregation=mean \
+set_encoder.aggregation=max,mean \
 set_encoder.use_context_size=False \
-control_and_hyper_net.use_hyper_net=True \
-control_and_hyper_net.is_cross_attentive=True,False \
-control_and_hyper_net.num_heads=1,2,3 \
-training.max_clip_norm=0.5 \
-training.alpha=None,1.0,0.8,0.6 \
-wandb.project=dvi-for-bml-cross-attn \
+set_encoder.is_attentive=True,False \
+control_and_hyper_net.use_hyper_net=True,False \
+control_and_hyper_net.is_cross_attentive=False \
+training.max_clip_norm=0.2 \
+wandb.project=dvi-for-bml-latent-space \
 # training.num_epochs=1 \
+# control_and_hyper_net.num_heads=1,2,3 \
