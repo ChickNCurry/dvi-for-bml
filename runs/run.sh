75b55e436d6a6e86a820ede5c2b17fd8f0158391
Maximilian.Burzer <maxi.burzer@gmail.com>
2024-12-09 10:17:41 +0100

    fix config

--- runs/run.sh
+++ runs/run.sh
@@ -4,10 +4,10 @@ which python
 
 python run.py --multirun \
 hydra=gpu \
-wandb.project=cdvi-bml-alternating \
+wandb.project=cdvi-bml-alternating-new \
 common.h_dim=64 \
 set_encoder.aggregation=max,mean \
 set_encoder.is_attentive=True,False \
 training.max_clip_norm=null,0.5 \
-training.alternating_ratio=null,0.3,0.5,0.7 \
+training.alternating_ratio=null,0.1,0.2,0.3 \
 training.alpha=null,1.0 \
