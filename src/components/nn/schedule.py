d32401a30deb30c1e13313bdd94e745fa20a2e97
Maximilian.Burzer <maxi.burzer@gmail.com>
2025-01-04 11:49:21 +0100

    add new schedules

--- src/components/nn/schedule.py
+++ src/components/nn/schedule.py
@@ -1,63 +1,244 @@
 import math
 from abc import ABC, abstractmethod
+from typing import List
 
 import numpy as np
 import torch
 from torch import Tensor, nn
 
 
-class Schedule(ABC, nn.Module):
+class NoiseSchedule(nn.Module):
     def __init__(self, z_dim: int, num_steps: int, device: torch.device) -> None:
-        super(Schedule, self).__init__()
 
-        self.z_dim = z_dim
+        # init amplitude with 0.1
+        self.amplitude = nn.Parameter(torch.ones((z_dim), device=device))
+
+        self.cosine_square_schedule: List[float] = [
+            np.square(np.cos((math.pi / 2) * (n / num_steps)))
+            for n in range(0, num_steps)
+        ]
+
+    def get(self, n: int) -> Tensor:
+        # transition from n-1 to n
+
+        noise = (
+            torch.nn.functional.softplus(self.amplitude)
+            * self.cosine_square_schedule[n - 1]
+        )
+        # start at amplitude
+        # never reaches 0
+
+        return noise
+
+
+class ContextualNoiseSchedule(nn.Module):
+    def __init__(
+        self,
+        h_dim: int,
+        z_dim: int,
+        non_linearity: str,
+        num_steps: int,
+    ) -> None:
+
+        self.amplitude_mlp = nn.Sequential(
+            nn.Linear(h_dim, h_dim),
+            getattr(nn, non_linearity)(),
+            nn.Linear(h_dim, z_dim),
+            nn.Softplus(),
+        )
+
+        # init amplitude_mlp such that amplitude is 1
+        nn.init.constant_(self.amplitude_mlp[-1].weight, 0)
+        nn.init.constant_(self.amplitude_mlp[-1].bias, 1)
+
+        self.cosine_square_schedule: List[float] = [
+            np.square(np.cos((math.pi / 2) * (n / num_steps)))
+            for n in range(0, num_steps)
+        ]
+
+    def update(self, r: Tensor) -> None:
+        self.amplitude = self.amplitude_mlp(r)
+
+    def get(self, n: int) -> Tensor:
+        # transition from n-1 to n
+
+        sigma: Tensor = self.amplitude * self.cosine_square_schedule[n - 1]
+        # start at amplitude
+        # never reaches 0
+
+        return sigma
+
+
+class StepSizeSchedule(nn.Module):
+    def __init__(self, z_dim: int, num_steps: int, device: torch.device) -> None:
+
+        # init amplitude with 0.1
+        self.amplitude = nn.Parameter(torch.ones((z_dim), device=device) * 0.1)
+
+        self.cosine_square_schedule: List[float] = [
+            np.square(np.cos((math.pi / 2) * (n / num_steps)))
+            for n in range(0, num_steps)
+        ][::-1]
+
+    def get(self, n: int) -> Tensor:
+        # transition from n-1 to n
+
+        step_size = (
+            torch.nn.functional.softplus(self.amplitude)
+            * self.cosine_square_schedule[n - 1]
+        )
+        # starts higher than 0
+        # reaches amplitude
+
+        return step_size
+
+
+class ContextualStepSizeSchedule(nn.Module):
+    def __init__(
+        self,
+        h_dim: int,
+        z_dim: int,
+        non_linearity: str,
+        num_steps: int,
+    ) -> None:
+
+        self.amplitude_mlp = nn.Sequential(
+            nn.Linear(h_dim, h_dim),
+            getattr(nn, non_linearity)(),
+            nn.Linear(h_dim, z_dim),
+            nn.Softplus(),
+        )
+
+        # init amplitude_mlp such that amplitude is 0.1
+        nn.init.constant_(self.amplitude_mlp[-1].weight, 0)
+        nn.init.constant_(self.amplitude_mlp[-1].bias, 0.1)
+
+        self.cosine_square_schedule: List[float] = [
+            np.square(np.cos((math.pi / 2) * (n / num_steps)))
+            for n in range(0, num_steps)
+        ][::-1]
+
+    def update(self, r: Tensor) -> None:
+        self.amplitude = self.amplitude_mlp(r)
+
+    def get(self, n: int) -> Tensor:
+        # transition from n-1 to n
+
+        step_size: Tensor = self.amplitude * self.cosine_square_schedule[n - 1]
+        # starts higher than 0
+        # reaches amplitude
+
+        return step_size
+
+
+class AnnealingSchedule(nn.Module):
+    def __init__(self, z_dim: int, num_steps: int, device: torch.device) -> None:
+
+        # init params such that betas increase linearly
+        self.params = nn.Parameter(
+            torch.ones((num_steps, z_dim), device=device) / num_steps
+        )
+
+    def get(self, n: int) -> Tensor:
+        # transition from n-1 to n
+
+        params = torch.nn.functional.softplus(self.params)
+        betas = torch.cumsum(params, dim=0) / torch.sum(params, dim=0)
+        beta = betas[n - 1]
+        # starts higher than 0
+        # reaches 1
+
+        return beta
+
+
+class ContextualAnnealingSchedule(nn.Module):
+    def __init__(
+        self,
+        h_dim: int,
+        z_dim: int,
+        non_linearity: str,
+        num_steps: int,
+    ) -> None:
+
+        self.params_mlp = nn.Sequential(
+            nn.Linear(num_steps * h_dim, num_steps * h_dim),
+            getattr(nn, non_linearity)(),
+            nn.Linear(num_steps * h_dim, num_steps * z_dim),
+            nn.Softplus(),
+        )
+
+        # init params_mlp corresponding to linearly increasing betas
+        nn.init.constant_(self.params_mlp[-1].weight, 0)
+        nn.init.constant_(self.params_mlp[-1].bias, 1 / num_steps)
+
         self.num_steps = num_steps
-        self.device = device
 
-    @abstractmethod
-    def get(self, t: int) -> Tensor:
-        pass
+    def update(self, r: Tensor) -> None:
+        r = r.expand(self.num_steps, -1).flatten()
+        self.params: Tensor = self.params_mlp(r).view(self.num_steps, -1)
 
+    def get(self, n: int) -> Tensor:
+        # transition from n-1 to n
 
-class AnnealingSchedule(Schedule):
-    def __init__(
-        self, z_dim: int, num_steps: int, device: torch.device, min: float
-    ) -> None:
-        super(AnnealingSchedule, self).__init__(z_dim, num_steps, device)
+        betas = torch.cumsum(self.params, dim=0) / torch.sum(self.params, dim=0)
+        beta = betas[n - 1]
+        # starts higher than 0
+        # reaches 1
 
-        self.schedule = nn.ParameterList(
-            [
-                nn.Parameter(torch.ones((self.z_dim), device=device) * beta)
-                for beta in np.linspace(min, 1 - min, num_steps)
-            ]
-        )
-
-    def get(self, t: int) -> Tensor:
-        entry: Tensor = self.schedule[t - 1]
-
-        return entry
+        return beta
 
 
-class CosineSchedule(Schedule):
-    def __init__(
-        self, z_dim: int, num_steps: int, device: torch.device, min: float
-    ) -> None:
-        super(CosineSchedule, self).__init__(z_dim, num_steps, device)
+# class Schedule(ABC, nn.Module):
+#     def __init__(self, z_dim: int, num_steps: int, device: torch.device) -> None:
+#         super(Schedule, self).__init__()
 
-        self.schedule = nn.ParameterList(
-            [
-                nn.Parameter(torch.ones((z_dim), device=device) * beta)
-                for beta in [
-                    (1 - min) * np.cos(math.pi * (1 - t) * 0.5) ** 2 + min
-                    for t in np.linspace(1, 0, num_steps)
-                ]
-            ]
-        )
+#         self.z_dim = z_dim
+#         self.num_steps = num_steps
+#         self.device = device
 
-    def get(self, t: int) -> Tensor:
-        entry: Tensor = self.schedule[t - 1]
+#     @abstractmethod
+#     def get(self, n: int) -> Tensor:
+#         pass
 
-        return entry
+# class AnnealingSchedule(Schedule):
+#     def __init__(
+#         self, z_dim: int, num_steps: int, device: torch.device, min: float
+#     ) -> None:
+#         super(AnnealingSchedule, self).__init__(z_dim, num_steps, device)
+
+#         self.schedule = nn.ParameterList(
+#             [
+#                 nn.Parameter(torch.ones((self.z_dim), device=device) * beta)
+#                 for beta in np.linspace(min, 1 - min, num_steps)
+#             ]
+#         )
+
+#     def get(self, t: int) -> Tensor:
+#         entry: Tensor = self.schedule[t - 1]
+
+#         return entry
+
+
+# class CosineSchedule(Schedule):
+#     def __init__(
+#         self, z_dim: int, num_steps: int, device: torch.device, min: float
+#     ) -> None:
+#         super(CosineSchedule, self).__init__(z_dim, num_steps, device)
+
+#         self.schedule = nn.ParameterList(
+#             [
+#                 nn.Parameter(torch.ones((z_dim), device=device) * beta)
+#                 for beta in [
+#                     (1 - min) * np.cos(math.pi * (1 - t) * 0.5) ** 2 + min
+#                     for t in np.linspace(1, 0, num_steps)
+#                 ]
+#             ]
+#         )
+
+#     def get(self, t: int) -> Tensor:
+#         entry: Tensor = self.schedule[t - 1]
+
+#         return entry
 
 
 # class ContextualSchedule(Schedule):
