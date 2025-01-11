8560909dde03a4bb0b30d1e29d9e5b7c31c9cef3
Max Burzer <maxi.burzer@gmail.com>
2025-01-11 13:41:03 +0100

    update cfgs

--- runs/scripts/run.sh
+++ runs/scripts/run.sh
@@ -8,5 +8,5 @@ wandb.project=cluster \
 common.variant=aggr,bca,mha \
 common.non_linearity=GELU,SiLU \
 common.self_attn_num_heads=null,1,2 \
-training.max_clip_norm=null,0.2 \
+training.max_clip_norm=null,0.5 \
 training.alpha=null,1.0 \
