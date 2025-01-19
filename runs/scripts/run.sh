26dddffa9fa9c413801dbcaca8148d05827db3c2
Max Burzer <maxi.burzer@gmail.com>
2025-01-19 16:01:00 +0100

    add cluster changes

--- runs/scripts/run.sh
+++ runs/scripts/run.sh
@@ -6,8 +6,11 @@ python ../run.py --multirun \
 hydra=gpu \
 wandb.project=cluster-sine \
 benchmark._target_=metalearning_benchmarks.sinusoid1d_benchmark.Sinusoid1D \
-common.variant=aggr,bca,mha \
+training.seed=1,2,3 \
+training.max_clip_norm=1.0,0.1 \
+common.self_attn_num_heads=null,1 \
 common.non_linearity=GELU,SiLU \
-common.self_attn_num_heads=null,1,2 \
-training.max_clip_norm=null,0.5 \
-training.alpha=null,1.0 \
+common.variant=aggr,bca,mha \
+
+# training.alpha=null,1.0 \
+# common.self_attn_num_heads=1 \
--- runs/scripts/run_dev.sh
+++ runs/scripts/run_dev.sh
@@ -5,8 +5,8 @@ which python
 python ../run.py --multirun \
 hydra=gpu_dev \
 wandb.project=cluster-test \
-common.variant=aggr,bca,mha \
-common.non_linearity=GELU,SiLU \
-common.self_attn_num_heads=null,1,2 \
 training.max_clip_norm=null,0.5 \
 training.alpha=null,1.0 \
+common.self_attn_num_heads=null,1,2 \
+common.non_linearity=GELU,SiLU \
+common.variant=aggr,bca,mha \
