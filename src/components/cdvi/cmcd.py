fix numerical issues in components and eval

src/components/cdvi/cmcd.py
@@ -53,7 +53,7 @@ class CMCD(CDVI):
         control_n = self.control(n, z, self.r, self.mask, None)
         # (batch_size, num_subtasks, z_dim)
 
-        z_mu = z + (var_n * score + control_n) * delta_t_n
+        z_mu = z + (var_n * score + torch.sqrt(var_n) * control_n) * delta_t_n
         z_sigma = torch.sqrt(2 * var_n * delta_t_n)
         # (batch_size, num_subtasks, z_dim)
 
@@ -69,7 +69,7 @@ class CMCD(CDVI):
         control_n = self.control(n, z, self.r, self.mask, None)
         # (batch_size, num_subtasks, z_dim)
 
-        z_mu = z - (var_n * score + control_n) * delta_t_n
+        z_mu = z - (var_n * score + torch.sqrt(var_n) * control_n) * delta_t_n
         z_sigma = torch.sqrt(2 * var_n * delta_t_n)
         # (batch_size, num_subtasks, z_dim)
 
@@ -94,8 +94,8 @@ class CMCD(CDVI):
 
         score_n = torch.nan_to_num(score_n)
 
-        grad_norm = score_n.norm(p=2)
-        if grad_norm > 1:
-            score_n = score_n * (1 / grad_norm)
+        # grad_norm = score_n.norm(p=2)
+        # if grad_norm > 1:
+        #     score_n = score_n * (1 / grad_norm)
 
         return score_n

src/components/decoder/decoder.py
@@ -1,4 +1,5 @@
 from torch import Tensor, nn
+import torch
 from torch.distributions import Distribution, Normal
 
 
@@ -29,7 +30,7 @@ class Decoder(nn.Module):
         )
 
         self.proj_y_mu = nn.Linear(h_dim, y_dim)
-        self.proj_y_w = nn.Linear(h_dim, y_dim)
+        self.proj_y_logvar = nn.Linear(h_dim, y_dim)
 
     def forward(
         self,
@@ -43,15 +44,12 @@ class Decoder(nn.Module):
         z = z.unsqueeze(2).expand(-1, -1, x_target.shape[2], -1)
         # (batch_size, num_subtasks, target_size, z_dim)
 
-        z = self.proj_z(z)
-        x = self.proj_x(x_target)
-        # (batch_size, num_subtasks, target_size, h_dim)
-
-        h = self.mlp(x + z)
+        h = self.mlp(self.proj_x(x_target) + self.proj_z(z))
         # (batch_size, num_subtasks, target_size, h_dim)
 
         y_mu = self.proj_y_mu(h)
-        y_sigma = 0.1 + 0.9 * nn.functional.softplus(self.proj_y_w(h))
+        # y_sigma = 0.1 + 0.9 * nn.functional.softplus(self.proj_y_w(h))
+        y_sigma = torch.exp(0.5 * self.proj_y_logvar(h))
         # (batch_size, num_subtasks, target_size, y_dim)
 
         return Normal(y_mu, y_sigma)  # type: ignore

src/components/decoder/dp_decoder.py
@@ -1,3 +1,4 @@
+import torch
 from torch import Tensor, nn
 from torch.distributions import Distribution, Normal
 
@@ -44,7 +45,7 @@ class DualPathDecoder(nn.Module):
         )
 
         self.proj_y_mu = nn.Linear(h_dim, y_dim)
-        self.proj_y_w = nn.Linear(h_dim, y_dim)
+        self.proj_y_logvar = nn.Linear(h_dim, y_dim)
 
     def forward(
         self,
@@ -125,7 +126,8 @@ class DualPathDecoder(nn.Module):
         # (batch_size, num_subtasks, target_size, h_dim)
 
         y_mu = self.proj_y_mu(h)
-        y_sigma = 0.1 + 0.9 * nn.Softplus()(self.proj_y_w(h))
+        # y_sigma = 0.1 + 0.9 * nn.Softplus()(self.proj_y_w(h))
+        y_sigma = torch.exp(0.5 * self.proj_y_logvar(h))
         # (batch_size, num_subtasks, target_size, y_dim)
 
         # y_mu = torch.nan_to_num(y_mu)

src/components/encoder/bca_encoder.py
@@ -34,7 +34,7 @@ class BCAEncoder(BaseEncoder):
         )
 
         self.proj_r = nn.Linear(h_dim, h_dim)
-        self.proj_r_var = nn.Linear(h_dim, h_dim)
+        self.proj_r_logvar = nn.Linear(h_dim, h_dim)
 
     def forward(self, context: Tensor, mask: Tensor | None) -> Tuple[Tensor, Tensor]:
         # (batch_size, num_subtasks, context_size, c_dim)
@@ -70,7 +70,8 @@ class BCAEncoder(BaseEncoder):
 
         h = self.mlp(h)
         r = self.proj_r(h)
-        r_var = nn.functional.softplus(self.proj_r_var(h))
+        # r_var = nn.functional.softplus(self.proj_r_var(h))
+        r_var = torch.exp(self.proj_r_logvar(h))
         # (batch_size, num_subtasks, context_size, h_dim)
 
         z_var_0 = torch.ones((batch_size, num_subtasks, self.h_dim), device=h.device)

src/components/schedule/noise_schedule.py
@@ -26,7 +26,7 @@ class NoiseSchedule(BaseSchedule):
             .unsqueeze(1)
             .expand(self.num_entries, z_dim),
             requires_grad=requires_grad,
-        )
+        )  # (num_entries, z_dim)
 
     def get(self, n: int) -> Tensor:
         var_n = torch.nn.functional.softplus(self.noise_schedule[n, :])

src/eval/grid.py
@@ -26,20 +26,19 @@ def eval_kde_on_grid(
 ) -> NDArray[np.float32]:
     # (dim1, dim2, ..., z_dim), (num_samples, z_dim)
 
-    eps = 1e-10
-
     grid_flat = grid.reshape(-1, grid.shape[-1])
     # (dim1 * dim2 * ..., z_dim)
 
     kde = gaussian_kde(samples.T)
-    vals: NDArray[np.float32] = kde(grid_flat.T)
-    vals = vals / (np.sum(vals) + eps)
+    probs: NDArray[np.float32] = kde(grid_flat.T)
+    log_probs = np.log(np.clip(probs, a_min=1e-300, a_max=None))
+    log_probs = log_probs - logsumexp(log_probs)
     # (dim1 * dim2 * ...)
 
-    vals = vals.reshape(grid.shape[:-1])
+    log_probs = log_probs.reshape(grid.shape[:-1])
     # (dim1, dim2, ...)
 
-    return vals
+    return log_probs
 
 
 def eval_hist_on_grid(
@@ -47,21 +46,20 @@ def eval_hist_on_grid(
 ) -> NDArray[np.float32]:
     # (num_samples, z_dim)
 
-    eps = 1e-10
-
     step_sizes = [(max - min) / num_cells for min, max in ranges]
     outer_edges = [
         (min - step_size, max + step_size)
         for (min, max), step_size in zip(ranges, step_sizes)
     ]
 
-    vals, _ = np.histogramdd(samples, bins=num_cells, range=outer_edges, density=True)
-    vals = vals / (np.sum(vals) + eps)
+    probs, _ = np.histogramdd(samples, bins=num_cells, range=outer_edges, density=True)
+    log_probs: NDArray[np.float32] = np.log(np.clip(probs, a_min=1e-300, a_max=None))
+    log_probs = log_probs - logsumexp(log_probs)
     # (dim1, dim2, ...)
 
-    vals = vals.T  # TODO: why transpose needed?
+    log_probs = log_probs.T  # TODO: why transpose needed?
 
-    return vals
+    return log_probs
 
 
 def eval_dist_on_grid(
@@ -75,22 +73,14 @@ def eval_dist_on_grid(
     grid_tensor = torch.from_numpy(grid_flat).float().to(device).unsqueeze(0)
     # (1, dim1 * dim2 * ..., z_dim)
 
-    vals = dist.log_prob(grid_tensor).exp().sum(-1).detach().cpu().numpy()
-
-    # print(np.sum(vals, axis=-1))
-    # vals = vals.astype(np.float128, casting="unsafe")
-
-    vals = vals / (np.sum(vals, axis=-1, keepdims=True))
+    log_probs = dist.log_prob(grid_tensor).sum(-1).detach().cpu().numpy()
+    log_probs = log_probs - logsumexp(log_probs)
     # (batch_size, dim1 * dim2 * ...)
 
-    # print(np.sum(vals, axis=-1))
-
-    # vals = vals.astype(np.float128, casting="unsafe")
-
-    vals = vals.reshape(vals.shape[0], *grid.shape[:-1])
+    log_probs = log_probs.reshape(log_probs.shape[0], *grid.shape[:-1])
     # (batch_size, dim1, dim2, ...)
 
-    return vals
+    return log_probs
 
 
 def eval_score_on_grid(
@@ -125,24 +115,21 @@ def eval_score_on_grid(
     return grad
 
 
-def sample_from_vals(
-    grid: NDArray[np.float32], vals: NDArray[np.float32], num_samples: int
+def sample_from_log_probs(
+    grid: NDArray[np.float32], log_probs: NDArray[np.float32], num_samples: int
 ) -> NDArray[np.float32]:
     # (dim1, dim2, ...)
 
-    eps = 1e-10
-
-    flat_vals = vals.reshape(-1)
+    flat_log_probs = log_probs.reshape(-1)
+    flat_probs = np.exp(flat_log_probs)
     # (dim1 * dim2 * ...)
 
-    # flat_vals = flat_vals / (np.sum(flat_vals) + eps)
-
-    # print(np.sum(flat_vals))
-
-    flat_indices = np.random.choice(flat_vals.shape[0], size=num_samples, p=flat_vals)
+    flat_indices = np.random.choice(
+        flat_log_probs.shape[0], size=num_samples, p=flat_probs
+    )
     # (num_samples)
 
-    grid_indices = np.unravel_index(flat_indices, vals.shape)
+    grid_indices = np.unravel_index(flat_indices, log_probs.shape)
     samples = grid[grid_indices]
     # (num_samples, z_dim)
 
@@ -150,35 +137,34 @@ def sample_from_vals(
 
 
 def compute_jsd(
-    p_vals: NDArray[np.float32],
-    q_vals: NDArray[np.float32],
+    p_log_probs: NDArray[np.float32],
+    q_log_probs: NDArray[np.float32],
 ) -> Any:
     # (dim1, dim2, ...)
-    # (dim1, dim2, ...)
 
-    eps = 1e-10
+    p_probs, q_probs = np.exp(p_log_probs), np.exp(q_log_probs)
+    m_probs = 0.5 * (p_probs + q_probs)
 
-    m_vals = 0.5 * (p_vals + q_vals) + eps
+    m_log_probs = np.log(m_probs)
 
-    jsd = 0.5 * np.sum(
-        p_vals * np.log((p_vals / m_vals) + eps)
-        + q_vals * np.log((q_vals / m_vals) + eps)
-    )
+    kl_p_m = p_probs * (p_log_probs - m_log_probs)
+    kl_q_m = q_probs * (q_log_probs - m_log_probs)
+
+    jsd = 0.5 * np.sum(kl_p_m + kl_q_m)
 
     return jsd
 
 
 def compute_bd(
-    p_vals: NDArray[np.float32],
-    q_vals: NDArray[np.float32],
+    p_log_probs: NDArray[np.float32],
+    q_log_probs: NDArray[np.float32],
 ) -> Any:
     # (dim1, dim2, ...)
-    # (dim1, dim2, ...)
 
-    eps = 1e-10
+    p_probs, q_probs = np.exp(p_log_probs), np.exp(q_log_probs)
 
-    bc = np.sum(np.sqrt(p_vals * q_vals))
-    bd = -np.log(bc + eps)
+    bc = np.sum(np.sqrt(p_probs * q_probs))
+    bd = -np.log(bc)
 
     return bd
 

src/eval/visualize.py
@@ -17,7 +17,7 @@ from src.eval.grid import (
     eval_dist_on_grid,
     eval_hist_on_grid,
     eval_score_on_grid,
-    sample_from_vals,
+    sample_from_log_probs,
 )
 
 
@@ -96,18 +96,18 @@ def visualize_dvinp_both(
         grid = create_grid(ranges, num_cells)
 
         # dvi_vals = eval_kde_on_grid(grid, z_samples[-1].cpu().detach().numpy())
-        dvi_vals = eval_hist_on_grid(
+        dvi_log_probs = eval_hist_on_grid(
             z_samples[-1].reshape(-1, z_samples[-1].shape[-1]).cpu().detach().numpy(),
             ranges,
             num_cells,
         )
 
-        target_vals = eval_dist_on_grid(grid, target, device=device).squeeze(0)
+        target_log_probs = eval_dist_on_grid(grid, target, device=device).squeeze(0)
 
         if show_score:
             score_vals = eval_score_on_grid(grid, target, device=device)
 
-        target_samples_np = sample_from_vals(grid, target_vals, num_samples)
+        target_samples_np = sample_from_log_probs(grid, target_log_probs, num_samples)
         target_samples = torch.from_numpy(target_samples_np).unsqueeze(0).to(device)
 
         y_dist_test: Distribution = dvinp.decoder(target_samples, x_data)
@@ -136,10 +136,10 @@ def visualize_dvinp_both(
             )
 
         ax[1].set_title("$q_\phi(z_T|z_{0:T-1}, D^c)$")
-        ax[1].contourf(grid[:, :, 0], grid[:, :, 1], dvi_vals, cmap=cm.coolwarm)  # type: ignore
+        ax[1].contourf(grid[:, :, 0], grid[:, :, 1], np.exp(dvi_log_probs), cmap=cm.coolwarm)  # type: ignore
 
         ax[2].set_title("$p_\\theta(y_{1:N}|x_{1:N},z_T)p_\\theta(z_T)$")
-        ax[2].contourf(grid[:, :, 0], grid[:, :, 1], target_vals, cmap=cm.coolwarm)  # type: ignore
+        ax[2].contourf(grid[:, :, 0], grid[:, :, 1], np.exp(target_log_probs), cmap=cm.coolwarm)  # type: ignore
 
         if show_score:
             ax[1].quiver(

src/utils/load_dvinp.py
@@ -9,8 +9,8 @@ from hydra.utils import instantiate
 from metalearning_benchmarks import MetaLearningBenchmark  # type: ignore
 from omegaconf import DictConfig
 from torch.optim.adamw import AdamW
-from torch.utils.data import DataLoader, random_split
 from torch.optim.lr_scheduler import CosineAnnealingLR, CosineAnnealingWarmRestarts
+from torch.utils.data import DataLoader, random_split
 
 from src.components.cdvi.dis import DIS
 from src.components.control.aggr_control import AggrControl
@@ -40,7 +40,8 @@ from src.utils.datasets import MetaLearningDataset
 
 
 class ContextualizationVariant(Enum):
-    AGGR = "aggr"
+    MEAN = "mean"
+    MAX = "max"
     BCA = "bca"
     MHA = "mha"
 
@@ -88,152 +89,152 @@ def load_dvinp(
         generator=g,
     )
 
-    variant = ContextualizationVariant(cfg.common.variant)
+    variant = ContextualizationVariant(cfg.model.variant)
 
     match variant:
-        case ContextualizationVariant.AGGR:
+        case ContextualizationVariant.MEAN | ContextualizationVariant.MAX:
 
             encoder = AggrEncoder(
-                c_dim=cfg.common.c_dim,
-                h_dim=cfg.common.h_dim,
-                num_layers=cfg.common.num_layers,
-                non_linearity=cfg.common.non_linearity,
-                num_heads=cfg.common.self_attn_num_heads,
-                aggregation=Aggr(cfg.common.aggregation),
-                max_context_size=cfg.common.max_context_size,
+                c_dim=cfg.model.c_dim,
+                h_dim=cfg.model.h_dim,
+                num_layers=cfg.model.num_layers,
+                non_linearity=cfg.model.non_linearity,
+                num_heads=cfg.model.self_attn_num_heads,
+                aggregation=Aggr(ContextualizationVariant(variant).value),
+                max_context_size=cfg.model.max_context_size,
             )
 
             control = AggrControl(
-                h_dim=cfg.common.h_dim,
-                z_dim=cfg.common.z_dim,
-                num_steps=cfg.common.num_steps,
-                num_layers=cfg.common.num_layers,
-                non_linearity=cfg.common.non_linearity,
-                use_score=cfg.common.use_score,
+                h_dim=cfg.model.h_dim,
+                z_dim=cfg.model.z_dim,
+                num_steps=cfg.model.num_steps,
+                num_layers=cfg.model.num_layers,
+                non_linearity=cfg.model.non_linearity,
+                use_score=cfg.model.use_score,
             )
 
             noise_schedule = AggrNoiseSchedule(
-                z_dim=cfg.common.z_dim,
-                h_dim=cfg.common.h_dim,
-                non_linearity=cfg.common.non_linearity,
-                num_steps=cfg.common.num_steps,
+                z_dim=cfg.model.z_dim,
+                h_dim=cfg.model.h_dim,
+                non_linearity=cfg.model.non_linearity,
+                num_steps=cfg.model.num_steps,
                 device=device,
             )
 
             annealing_schedule = AggrAnnealingSchedule(
-                h_dim=cfg.common.h_dim,
-                non_linearity=cfg.common.non_linearity,
-                num_steps=cfg.common.num_steps,
+                h_dim=cfg.model.h_dim,
+                non_linearity=cfg.model.non_linearity,
+                num_steps=cfg.model.num_steps,
                 device=device,
             )
 
         case ContextualizationVariant.BCA:
 
             encoder = BCAEncoder(
-                c_dim=cfg.common.c_dim,
-                h_dim=cfg.common.h_dim,
-                num_layers=cfg.common.num_layers,
-                non_linearity=cfg.common.non_linearity,
-                num_heads=cfg.common.self_attn_num_heads,
+                c_dim=cfg.model.c_dim,
+                h_dim=cfg.model.h_dim,
+                num_layers=cfg.model.num_layers,
+                non_linearity=cfg.model.non_linearity,
+                num_heads=cfg.model.self_attn_num_heads,
             )
 
             control = BCAControl(
-                h_dim=cfg.common.h_dim,
-                z_dim=cfg.common.z_dim,
-                num_steps=cfg.common.num_steps,
-                num_layers=cfg.common.num_layers,
-                non_linearity=cfg.common.non_linearity,
-                use_score=cfg.common.use_score,
+                h_dim=cfg.model.h_dim,
+                z_dim=cfg.model.z_dim,
+                num_steps=cfg.model.num_steps,
+                num_layers=cfg.model.num_layers,
+                non_linearity=cfg.model.non_linearity,
+                use_score=cfg.model.use_score,
             )
 
             noise_schedule = BCANoiseSchedule(
-                z_dim=cfg.common.z_dim,
-                h_dim=cfg.common.h_dim,
-                non_linearity=cfg.common.non_linearity,
-                num_steps=cfg.common.num_steps,
+                z_dim=cfg.model.z_dim,
+                h_dim=cfg.model.h_dim,
+                non_linearity=cfg.model.non_linearity,
+                num_steps=cfg.model.num_steps,
                 device=device,
             )
 
             annealing_schedule = BCAAnnealingSchedule(
-                h_dim=cfg.common.h_dim,
-                non_linearity=cfg.common.non_linearity,
-                num_steps=cfg.common.num_steps,
+                h_dim=cfg.model.h_dim,
+                non_linearity=cfg.model.non_linearity,
+                num_steps=cfg.model.num_steps,
                 device=device,
             )
 
         case ContextualizationVariant.MHA:
 
             encoder = MHAEncoder(
-                c_dim=cfg.common.c_dim,
-                h_dim=cfg.common.h_dim,
-                num_layers=cfg.common.num_layers,
-                non_linearity=cfg.common.non_linearity,
-                num_heads=cfg.common.self_attn_num_heads,
+                c_dim=cfg.model.c_dim,
+                h_dim=cfg.model.h_dim,
+                num_layers=cfg.model.num_layers,
+                non_linearity=cfg.model.non_linearity,
+                num_heads=cfg.model.self_attn_num_heads,
             )
 
             control = MHAControl(
-                h_dim=cfg.common.h_dim,
-                z_dim=cfg.common.z_dim,
-                num_steps=cfg.common.num_steps,
-                num_layers=cfg.common.num_layers,
-                non_linearity=cfg.common.non_linearity,
-                use_score=cfg.common.use_score,
-                num_heads=cfg.common.cross_attn_num_heads,
+                h_dim=cfg.model.h_dim,
+                z_dim=cfg.model.z_dim,
+                num_steps=cfg.model.num_steps,
+                num_layers=cfg.model.num_layers,
+                non_linearity=cfg.model.non_linearity,
+                use_score=cfg.model.use_score,
+                num_heads=cfg.model.cross_attn_num_heads,
             )
 
             noise_schedule = MHANoiseSchedule(
-                z_dim=cfg.common.z_dim,
-                h_dim=cfg.common.h_dim,
-                non_linearity=cfg.common.non_linearity,
-                num_steps=cfg.common.num_steps,
+                z_dim=cfg.model.z_dim,
+                h_dim=cfg.model.h_dim,
+                non_linearity=cfg.model.non_linearity,
+                num_steps=cfg.model.num_steps,
                 device=device,
-                num_heads=cfg.common.cross_attn_num_heads,
+                num_heads=cfg.model.cross_attn_num_heads,
             )
 
             annealing_schedule = MHAAnnealingSchedule(
-                h_dim=cfg.common.h_dim,
-                non_linearity=cfg.common.non_linearity,
-                num_steps=cfg.common.num_steps,
+                h_dim=cfg.model.h_dim,
+                non_linearity=cfg.model.non_linearity,
+                num_steps=cfg.model.num_steps,
                 device=device,
-                num_heads=cfg.common.cross_attn_num_heads,
+                num_heads=cfg.model.cross_attn_num_heads,
             )
 
-    step_size_schedule = StepSizeSchedule(
-        num_steps=cfg.common.num_steps,
-        device=device,
-    )
-
-    if not cfg.common.contextual_schedules:
+    if not cfg.model.contextual_schedules:
 
         noise_schedule = NoiseSchedule(
-            z_dim=cfg.common.z_dim,
-            num_steps=cfg.common.num_steps,
+            z_dim=cfg.model.z_dim,
+            num_steps=cfg.model.num_steps,
             device=device,
         )
 
         annealing_schedule = AnnealingSchedule(
-            num_steps=cfg.common.num_steps,
+            num_steps=cfg.model.num_steps,
             device=device,
         )
 
+    step_size_schedule = StepSizeSchedule(
+        num_steps=cfg.model.num_steps,
+        device=device,
+    )
+
     cdvi = DIS(
-        z_dim=cfg.common.z_dim,
-        num_steps=cfg.common.num_steps,
+        z_dim=cfg.model.z_dim,
+        num_steps=cfg.model.num_steps,
         control=control,
         step_size_schedule=step_size_schedule,
         noise_schedule=noise_schedule,
         annealing_schedule=annealing_schedule,
-        use_score=cfg.common.use_score,
+        use_score=cfg.model.use_score,
         device=device,
     )
 
     decoder = Decoder(
-        x_dim=cfg.common.x_dim,
-        z_dim=cfg.common.z_dim,
-        h_dim=cfg.common.h_dim,
-        y_dim=cfg.common.y_dim,
-        num_layers=cfg.common.num_layers,
-        non_linearity=cfg.common.non_linearity,
+        x_dim=cfg.model.x_dim,
+        z_dim=cfg.model.z_dim,
+        h_dim=cfg.model.h_dim,
+        y_dim=cfg.model.y_dim,
+        num_layers=cfg.model.num_layers,
+        non_linearity=cfg.model.non_linearity,
     )
 
     dvinp = DVINP(
@@ -251,12 +252,12 @@ def load_dvinp(
 
     optimizer = AdamW(params=params, lr=cfg.training.learning_rate)
 
-    scheduler = CosineAnnealingWarmRestarts(
-        optimizer=optimizer,
-        T_0=5,
-        T_mult=2,
-        eta_min=cfg.training.learning_rate * 0.01,
-    )
+    # scheduler = CosineAnnealingWarmRestarts(
+    #     optimizer=optimizer,
+    #     T_0=5,
+    #     T_mult=2,
+    #     eta_min=cfg.training.learning_rate * 0.01,
+    # )
 
     trainer = BetterDVINPTrainer(
         device=device,