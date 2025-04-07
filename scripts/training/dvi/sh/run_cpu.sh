b05e907ea4887d2fb5f9312f9461e9482f4e3959
Maximilian.Burzer <maxi.burzer@gmail.com>
2025-04-07 12:17:26 +0200

    remove mhca from sh

--- scripts/training/dvi/sh/run_cpu.sh
+++ scripts/training/dvi/sh/run_cpu.sh
@@ -7,7 +7,7 @@ which python
 python ../run_dvi.py --multirun \
 hydra=cpu \
 model.model_variant=dis,dis_score \
-model.context_variant=mean,bca,mhca \
+model.context_variant=mean,bca \
 model.contextual_schedules=true,false \
 model.noise_variant=free,constr \
 model.max_context_size=null,10 \
